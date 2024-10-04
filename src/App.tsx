770f504c969bd95d62541a63be94fc9eac187da5
ratan <your-email@example.com>
2024-10-04 20:49:55 +0600

    anoter type added

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import User from './components/User';
 function App() {
   return (
     <div className="App">
-      <User name="Ratan chakma" age={23} />
+      <User name="Ratan chakma" age={23} isStudent="true" language='Chakma' />
     </div>
   );
 }
--- src/components/User.tsx
+++ src/components/User.tsx
@@ -12,13 +12,27 @@
 // export default User;
 
 // another way
-type userProps = {name:string,age:number}
+// type userProps = {name:string,age:number}
 
-const User = (props:userProps) => {
+// const User = (props:userProps) => {
+//     return (
+//         <div>
+//             <h1>{props.name}</h1>
+//             <p>{props.age}</p>
+//         </div>
+//     );
+// };
+
+// export default User;
+type userProps = {name:string,age:number,language:string,isStudent:string}
+
+const User = ({name,age,language,isStudent}:userProps) => {
     return (
         <div>
-            <h1>{props.name}</h1>
-            <p>{props.age}</p>
+            <h1>{name}</h1>
+            <h2>{language}</h2>
+            <h2>{isStudent}</h2>
+            <p>{age}</p>
         </div>
     );
 };
